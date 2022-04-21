1948d6f2d16bbe4a5de8bced434db6c315aabcbd
Kevin Clair <kevin@jackflaps.net>
2022-04-20 20:18:29 -0400

    remove Home and Browse links from second navbar

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,6 +1,3 @@
-Plugins::add_menu_item('/', 'Home', 0)
-Plugins::add_menu_item('#', 'Browse:', 1)
-
 unless AppConfig.has_key? :pui_classification_title_only
   AppConfig[:pui_classification_title_only] = false
 end
